bfa1fc4772950ad7a7a48c244abb4860bd91ced0
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2020-10-04 16:10:34 +1100

    Added cpp for generated map

---
+++ Source/AdvGamesProgramming/ProcedurallyGeneratedMap.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProcedurallyGeneratedMap.h"
+
+// Sets default values
+AProcedurallyGeneratedMap::AProcedurallyGeneratedMap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProcedurallyGeneratedMap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProcedurallyGeneratedMap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
