493142f53f2313ad317c084ffbd3c5a0d59d1d9a
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2020-10-05 15:47:49 +1100

    Changed Focus
    Adding armour pickups

--- Source/AdvGamesProgramming/ProcedurallyGeneratedMap.cpp
+++ Source/AdvGamesProgramming/ProcedurallyGeneratedMap.cpp
@@ -8,7 +8,7 @@ AProcedurallyGeneratedMap::AProcedurallyGeneratedMap()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	//Floor = 
 }
 
 // Called when the game starts or when spawned
