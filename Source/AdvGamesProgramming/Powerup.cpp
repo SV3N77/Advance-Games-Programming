5dcd49f92e16c4b972ca892554d69d3b2a0eb516
Izzeyy <65158484+Izzeyy@users.noreply.github.com>
2020-10-09 13:55:42 +1100

    re-added c++ comments

--- Source/AdvGamesProgramming/Powerup.cpp
+++ Source/AdvGamesProgramming/Powerup.cpp
@@ -12,14 +12,14 @@ void APowerup::OnGenerate()
 	// Jump Boost = 50% chance
 	// Speed Boost = 50% chance
 	// Powerup includes a 50/50 chance between a 'Jump Boost Powerup' and a 'Speed Boost Powerup'.
-	if (RandomValue <= 50)
+	if (RandomValue <= 50) // If 'RandomValue' is between 0 and 50 then...
 	{
 		Powerup = PowerupType::JUMPBOOST;
-		JumpBoostHeight = 700.0f; // Increase jump height
+		JumpBoostHeight = 700.0f; // Increase the jump height from 420.0f (default)
 	}
-	else if (RandomValue > 50 && RandomValue <= 100)
+	else if (RandomValue > 50 && RandomValue <= 100) // If 'RandomValue' is between 50 and 100 then...
 	{
 		Powerup = PowerupType::SPEEDBOOST;
-		SpeedBoostWalkSpeed = 1400.0f; // Increase walk speed
+		SpeedBoostWalkSpeed = 1350.0f; // Increase max walk speed from 900.0f (default)
 	}
 }
